cbf2ee6824e08d45eb7ee370c265a84e83c2979d
Matthes0 <matthes123321@gmail.com>
2022-05-22 14:46:21 +0200

    added models for death_counter

--- death_counter/models.py
+++ death_counter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    game_name = models.CharField(max_length=200)
+class Boss(models.Model):
+    boss_name = models.CharField(max_length=200)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    
