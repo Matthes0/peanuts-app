8f080107df57783efb6f7b106d01ff042e9a8f6e
Matthes0 <matthes123321@gmail.com>
2022-05-22 14:57:42 +0200

    added models for death_counter

--- death_counter/models.py
+++ death_counter/models.py
@@ -6,3 +6,7 @@ class Game(models.Model):
 class Boss(models.Model):
     boss_name = models.CharField(max_length=200)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+class Deaths(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    boss_name = models.ForeignKey(Boss, on_delete=models.CASCADE)
+    deaths = models.IntegerField(default=0)
