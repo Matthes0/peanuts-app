fcb02594ab43a2389850bae2d3d4bbd6c884f8c6
Matthes0 <matthes123321@gmail.com>
2022-05-22 19:28:00 +0200

    fixed error with verbose name in death counter config

--- death_counter/apps.py
+++ death_counter/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class DeathCounterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Death Counter'
+    name = 'death_counter'
+    verbose_name = "Death Counter"
