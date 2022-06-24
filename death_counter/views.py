8915fffaa4a3c1b6cc14f7b2a2ed5bd5fb4694c9
Matthes0 <matthes123321@gmail.com>
2022-06-24 16:27:11 +0200

    fixed forms

--- death_counter/views.py
+++ death_counter/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.forms import inlineformset_factory
-from .forms import GameForm, DeathForm, CreateUserForm
+from .forms import GameForm, DeathForm, CreateUserForm, BossForm
 # Create your views here.
 def stats(request):
     games = Game.objects.all()
@@ -19,17 +19,24 @@ def death_form(request, pk, ppk):
     game = Game.objects.get(id=pk)
     player = User.objects.get(id=ppk)
     deaths = Death.objects.filter(game=game).filter(player=player)
-    form = DeathForm(instance=player)
-    # deathFormset = DeathForm(queryset=Death.objects.none(), instance=deaths)
+    form = DeathForm(instance=player, initial={'game':game, 'player':player, 'deaths':deaths})
+    form.fields['boss_name'].queryset = Boss.objects.filter(game=pk)
     if request.method == 'POST':
-        form = DeathForm(request.POST, instance=player)
+        form = DeathForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('/death_counter/stats/'+pk)
 
     context = {'game':game, 'player':player, 'deaths':deaths, 'form': form}
     return render(request, 'death_counter/death_form.html', context)
 def game_boss(request, pk):
-
-    context = {}
+    game = Game.objects.get(id=pk)
+    form = BossForm(instance=game, initial={'game':game})
+    if request.method == 'POST':
+        print('Printing POST:', request.POST)
+        form = BossForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/death_counter/stats/'+pk)
+    context = {'form': form}
     return render(request, 'death_counter/game_boss.html', context)
